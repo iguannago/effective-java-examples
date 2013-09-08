10cfd246d0a930dcdda8883da9eb8e11800aeaef
David Crespo <davidcrespoarroyo@gmail.com>
2013-09-08 15:23:30 +0100

    Item1: Static factory method technique

---
+++ src/main/java/org/effectivejava/examples/chapter02/item1Test/Book.java
@@ -0,0 +1,19 @@
+package org.effectivejava.examples.chapter02.item1Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 08/09/13
+ * Time: 14:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+
+    /**
+     * Static factory method
+     * @return
+     */
+    public static Book book() {
+        return new Book();
+    }
+}
---
+++ src/main/java/org/effectivejava/examples/chapter02/item1Test/BookTest.java
@@ -0,0 +1,24 @@
+package org.effectivejava.examples.chapter02.item1Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 08/09/13
+ * Time: 14:40
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(JUnit4.class)
+public class BookTest {
+
+    @Test
+    public void classTest() {
+        Book book = new Book();
+        Book book2 = Book.book();
+        org.junit.Assert.assertNotNull(book);
+        org.junit.Assert.assertNotNull(book2);
+    }
+}
